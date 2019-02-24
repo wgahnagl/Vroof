387ef9f62539bd82c5f15b6bb7dca240c9ed83d0
dquach217 <dq6180@rit.edu>
2019-02-24 01:26:24 -0500

    add the logic for gravity

---
+++ Assets/gravity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravity : MonoBehaviour
+{
+    public ConstantForce grav;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grav = gameObject.AddComponent<ConstantForce>();
+        grav.force = new Vector3(0.0f, -9.81f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Rigidbody>().useGravity = false;
+    }
+}
