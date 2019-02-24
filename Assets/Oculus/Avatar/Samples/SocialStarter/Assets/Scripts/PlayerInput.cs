da4513f5d2817bd42fb98d76cd10c3c19c5cdf00
dquach217 <dq6180@rit.edu>
2019-02-24 00:57:11 -0500

    comment out

--- Assets/Oculus/Avatar/Samples/SocialStarter/Assets/Scripts/PlayerInput.cs
+++ Assets/Oculus/Avatar/Samples/SocialStarter/Assets/Scripts/PlayerInput.cs
@@ -1,50 +1,50 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿//using UnityEngine;
+//using System.Collections;
 
-public class PlayerInput : MonoBehaviour
-{
-    public new Animation animation;
-    // Use this for initialization
-    void Start()
-    {
+//public class PlayerInput : MonoBehaviour
+//{
+//    public new Animation animation;
+//    // Use this for initialization
+//    void Start()
+//    {
 
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Press A
-        if (OVRInput.Get(OVRInput.Button.One, OVRInput.Controller.RTouch)) {
-            while () {
-                animation.Play("feeding");
-                animation["wagging"].layer = 1;
-                animation.Play("wagging");
-                animation["wagging"].weight = 0.4f;
-            }
-        } else if (OVRInput.Get(OVRInput.Button.Three, OVRInput.Controller.LTouch)) {
-            // Press X
-            while ()
-            {
-                animation.Play("feeding");
-                animation["wagging"].layer = 1;
-                animation.Play("wagging");
-                animation["wagging"].weight = 0.4f;
-            }
-        } else if (OVRInput.Get(Axis1D.PrimaryIndexTrigger)) {
-            // Left Trigger is Held down
-            while(OVRInput.Get(Axis1D.PrimaryIndexTrigger) {
-                animation.Play("ears-down");
-                animation["wagging"].layer = 1;
-                animation.Play("wagging");
-                animation["wagging"].weight = 0.4f;
-                if () {
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        // Press A
+//        if (OVRInput.Get(OVRInput.Button.One, OVRInput.Controller.RTouch)) {
+//            while () {
+//                animation.Play("feeding");
+//                animation["wagging"].layer = 1;
+//                animation.Play("wagging");
+//                animation["wagging"].weight = 0.4f;
+//            }
+//        } else if (OVRInput.Get(OVRInput.Button.Three, OVRInput.Controller.LTouch)) {
+//            // Press X
+//            while ()
+//            {
+//                animation.Play("feeding");
+//                animation["wagging"].layer = 1;
+//                animation.Play("wagging");
+//                animation["wagging"].weight = 0.4f;
+//            }
+//        } else if (OVRInput.Get(Axis1D.PrimaryIndexTrigger)) {
+//            // Left Trigger is Held down
+//            while(OVRInput.Get(Axis1D.PrimaryIndexTrigger) {
+//                animation.Play("ears-down");
+//                animation["wagging"].layer = 1;
+//                animation.Play("wagging");
+//                animation["wagging"].weight = 0.4f;
+//                if () {
 
-                }
-            }
-        } else if(OVRInput.Get(Axis1D.SecondaryIndexTrigger)){
-            while(  ) {
+//                }
+//            }
+//        } else if(OVRInput.Get(Axis1D.SecondaryIndexTrigger)){
+//            while(  ) {
                 
-            }
-        }
-    }
-}
+//            }
+//        }
+//    }
+//}
