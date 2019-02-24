90400db99a8a4904d3e3bbfe7f7c90c22f3d42c9
Skyler Jane Clark <skylerjaneclark@gmail.com>
2019-02-24 08:27:26 -0500

    ADDS DOGGO BARKIN

---
+++ Assets/Resources/miscScripts/DogScripts.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DogScripts : MonoBehaviour
+{
+    Animator animator;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+
+        animator.SetBool("walk", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+
+                    animator.SetBool("walk", false);
+
+                }
+            }
+                    else {
+            animator.SetBool("walk", true);
+
+        }
+        }
+
+    }
+
+    
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.CompareTag("PlayerHand"))
+        {
+            animator.SetBool("Wiggle", true);
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.CompareTag("PlayerHand"))
+        {
+            animator.SetBool("Wiggle", false);
+        }
+    }
+}
