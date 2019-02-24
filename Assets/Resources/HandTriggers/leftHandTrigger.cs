6553c17ae5c7b05da7117914c08cc69708b6bde8
Skyler Jane Clark <skylerjaneclark@gmail.com>
2019-02-24 02:45:07 -0500

    refines the triggers

--- Assets/Resources/HandTriggers/leftHandTrigger.cs
+++ Assets/Resources/HandTriggers/leftHandTrigger.cs
@@ -4,28 +4,11 @@ using UnityEngine;
 
 public class leftHandTrigger : MonoBehaviour
 {
-    private void Start()
-    {
-        Debug.Log("left locked and loaded");
-    }
     void OnTriggerEnter(Collider myTrigger)
     {
-
-        if (! myTrigger.gameObject.name.Equals("OVR PlayerController")) {
-            Debug.Log("collidedLeft, trigger");
-            Debug.Log(myTrigger.gameObject.name);
-
-        }
-
-
-        if (myTrigger.gameObject.name.Equals("sheeb"))
-        {
-            Debug.Log("patting the dog with left hand, reacting on name!");
-        }
         if (myTrigger.gameObject.tag.Equals("shibe"))
         {
             Debug.Log("patting the dog with left hand, reacting on tag!");
         }
-        Debug.Log("collidedLeft, trigger");
     }
 }
--- Assets/Resources/HandTriggers/rightHandTrigger.cs
+++ Assets/Resources/HandTriggers/rightHandTrigger.cs
@@ -4,29 +4,11 @@ using UnityEngine;
 
 public class rightHandTrigger : MonoBehaviour
 {
-    private void Start()
-    {
-        Debug.Log("right locked and loaded");
-    }
     void OnTriggerEnter(Collider myTrigger)
     { 
-        if (! myTrigger.gameObject.name.Equals("OVRPlayerController")) {
-            Debug.Log("collidedRight, trigger");
-            Debug.Log(myTrigger.gameObject.tag);
-        }
-    
-        if (myTrigger.gameObject.name.Equals("sheeb"))
-        {
-            Debug.Log("patting the dog with right hand, reacting on name!");
-        }
         if (myTrigger.gameObject.tag.Equals("shibe"))
         {
             Debug.Log("patting the dog with right hand, reacting on tag!");
         }
-    }
-
-    void OnCollisionEnter(Collision col)
-    {
-        Debug.Log("collidedRight, collision");
     }
 }
